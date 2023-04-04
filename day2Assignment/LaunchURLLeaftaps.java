01d59047ee08d08ab1631ef8ef5f30e7c6322598
MalarvizhiGanesan <129307629+MalarvizhiGanesan@users.noreply.github.com>
2023-04-05 01:14:50 +0800

    Add files via upload
    week2.day2.assignments

---
+++ day2Assignment/LaunchURLLeaftaps.java
@@ -0,0 +1,33 @@
+package week2.day2Assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchURLLeaftaps {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		//driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoCsr");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.id("label")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Malarvizhi");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Ganesan");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("malarvizhiganesan2@gmail.com");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("92654320");
+		driver.findElement(By.name("submitButton")).click();
+		Thread.sleep(1000);
+		
+
+	}
+
+}
